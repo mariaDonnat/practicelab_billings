b2e88427cc6b7bd8d34cd27c06c613de14826724
unknown <madonnarose.olidana.iics@ust.edu.ph>
2020-02-15 22:17:55 +0800

    js - added verify function

--- form.js
+++ form.js
@@ -20,3 +20,32 @@ function billingFunction()
       document.getElementById("billingZip").value = "";
     }
   }
+
+  function verify()
+  {
+    let shipname = document.getElementById("shippingName").value
+    let billname = document.getElementById("billingName").value 
+
+    let shipzip = document.getElementById("shippingZip").value
+    let billzip = document.getElementById("billingZip").value
+    if (shipname === billname && shipzip === billzip)
+    {
+      alert("Both shipping and billing information matches.");
+    }
+    else
+    {
+      if (shipname != billname && shipzip != billzip)
+      {
+        alert("Both shipping and billing informations doesn't match.");
+      }
+      else if (shipzip != billzip)
+      {
+        alert("The shipping and billing ZIP doesn't match.");
+      }
+      else
+      {
+        alert("The shipping and billing NAME doesn't match.");
+      }
+    }
+    
+  }
